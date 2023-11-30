7285114ca0f92e8bb777f517fc6d3a4c9bf26bfd
dg266 <daniele.galloppo@protonmail.com>
2023-11-30 12:25:10 +0100

    Add code to test the LED

---
+++ EmbeddedSystem.py
@@ -0,0 +1,23 @@
+try:
+    import RPi.GPIO as GPIO
+except:
+    import mock.GPIO as GPIO
+
+
+class EmbeddedSystem:
+    LED_PIN = 11
+
+    # Constructor
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+
+        # LED setup
+        GPIO.setup(self.LED_PIN, GPIO.OUT)
+
+    def turn_on_led(self):
+        GPIO.output(self.LED_PIN, GPIO.HIGH)
+        print("INFO: the LED has been turned on.")
+
+    def turn_off_led(self):
+        GPIO.output(self.LED_PIN, GPIO.LOW)
+        print("INFO: the LED has been turned off.")
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+from EmbeddedSystem import EmbeddedSystem
+import time
+
+# I have created an instance of the EmbeddedSystem class
+es = EmbeddedSystem()
+
+es.turn_on_led()
+time.sleep(1)
+es.turn_off_led()
+time.sleep(1)
+es.turn_on_led()
+time.sleep(1)
+es.turn_off_led()
