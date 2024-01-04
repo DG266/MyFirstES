210f72917a9c288bffedfdd6e9476293934464a9
dg266 <daniele.galloppo@protonmail.com>
2024-01-04 03:10:42 +0100

    Change sensor values printed on the LCD screen when pressing the button

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 try:
@@ -9,21 +10,54 @@ from EmbeddedSystem import EmbeddedSystem
 
 
 es = EmbeddedSystem()
+current_screen = 0
+current_screen_lock = threading.Lock()
+
+
+def show_temp_and_humidity():
+    es.print_lcd(f"Temp: {es.environment_temp:.2f},\nHum: {es.humidity:.2f}")
+
+
+def show_liquid_level_and_turbidity():
+    es.print_lcd(f"Liq. lvl: {es.is_liquid_level_good},\nNTU: {es.ntu_val:.2f}")
+
+
+def update_screen():
+    with current_screen_lock:
+        if current_screen == 0:
+            show_temp_and_humidity()
+        elif current_screen == 1:
+            show_liquid_level_and_turbidity()
 
 
 def button_event(channel):
+    global current_screen
     print('INFO: The button has been pressed (GPIO%d).' % channel)
-    # TODO
+    with current_screen_lock:
+        if current_screen == 0:
+            current_screen = 1
+            es.clear_lcd()
+            show_liquid_level_and_turbidity()
+        elif current_screen == 1:
+            current_screen = 0
+            es.clear_lcd()
+            show_temp_and_humidity()
 
 
 def loop():
+    global es, current_screen
+
     GPIO.add_event_detect(es.BUTTON_PIN, GPIO.FALLING, callback=button_event, bouncetime=500)
+
     while True:
+        # Read all sensors
         es.read_environment_temp_and_humidity()
-        es.print_lcd("Temp: %.2f,\nHum: %.2f" % (es.environment_temp, es.humidity))
         es.check_liquid_level()
         es.check_liquid_turbidity()
-        time.sleep(1)
+
+        update_screen()
+
+        time.sleep(0.25)
 
 
 if __name__ == '__main__':
